c6dfbe98beea02ff2122afcd77e96b761bf1d2c8
AnantSharma09 <robinjaiswal555@gmail.com>
2025-07-04 16:26:25 +0530

    Added signup views

--- auctions/views.py
+++ auctions/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if not (username and email and password):
+            return render(request, 'signup.html', {'error': 'All fields are required'})
+        
+        if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
+            return render(request, 'signup.html', {'error': 'Username or email already exists'})
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        return render(request, 'signup.html', {'success': 'User created successfully'})
+    
+    return render(request, 'signup.html')
